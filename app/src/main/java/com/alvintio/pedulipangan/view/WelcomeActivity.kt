90e0f8939c70b674986fbf953287f10b55852790
Alvin Tio <alvin10202@gmail.com>
2023-12-14 00:08:52 +0700

    animation

--- app/src/main/java/com/alvintio/pedulipangan/view/WelcomeActivity.kt
+++ app/src/main/java/com/alvintio/pedulipangan/view/WelcomeActivity.kt
@@ -1,9 +1,11 @@
 package com.alvintio.pedulipangan.view
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
-import com.alvintio.pedulipangan.R
 import com.alvintio.pedulipangan.databinding.ActivityWelcomeBinding
 import com.alvintio.pedulipangan.util.ViewUtils
 
@@ -16,11 +18,12 @@ class WelcomeActivity : AppCompatActivity() {
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        appIcon = findViewById(R.id.iv_welcome_image)
-        appIcon.setAnimation(R.raw.app_logo)
-        appIcon.playAnimation()
+//        appIcon = findViewById(R.id.iv_welcome_image)
+//        appIcon.setAnimation(R.raw.app_logo)
+//        appIcon.playAnimation()
 
         ViewUtils.setupFullScreen(this)
+        playAnimation()
         setupAction()
     }
 
@@ -31,5 +34,27 @@ class WelcomeActivity : AppCompatActivity() {
         binding.btnRegister.setOnClickListener {
             ViewUtils.moveActivity(this, RegisterActivity::class.java)
         }
+    }
+
+    private fun playAnimation() {
+        val icon = ObjectAnimator.ofFloat(binding.iconDescription, View.ALPHA, 1f).setDuration(300)
+        val title = ObjectAnimator.ofFloat(binding.tvTitle, View.ALPHA, 1f).setDuration(300)
+        val subtitle =
+            ObjectAnimator.ofFloat(binding.tvSubtitle, View.ALPHA, 1f).setDuration(300)
+        val register =
+            ObjectAnimator.ofFloat(binding.btnRegister, View.ALPHA, 1f).setDuration(300)
+        val login =
+            ObjectAnimator.ofFloat(binding.btnLogin, View.ALPHA, 1f).setDuration(300)
+
+        AnimatorSet().apply {
+            playSequentially(
+                icon,
+                title,
+                subtitle,
+                register,
+                login
+            )
+            startDelay = 100
+        }.start()
     }
 }
