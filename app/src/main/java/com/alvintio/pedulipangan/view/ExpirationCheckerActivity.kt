4ab36b04955f1f70e41103bf75a91b5105061010
edwardardian <edwardtayu4@gmail.com>
2023-12-16 13:47:45 +0700

    Menambahkan fitur ExpirationCheckerFruitActivity.

--- app/src/main/java/com/alvintio/pedulipangan/view/ExpirationCheckerActivity.kt
+++ app/src/main/java/com/alvintio/pedulipangan/view/ExpirationCheckerActivity.kt
@@ -1,226 +1,29 @@
 package com.alvintio.pedulipangan.view
 
-import android.Manifest
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Matrix
-import android.net.Uri
-import android.os.Bundle
-import android.os.Environment
-import android.provider.MediaStore
-import android.provider.SyncStateContract.Helpers
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.content.FileProvider
-import androidx.core.util.Consumer
-import androidx.exifinterface.media.ExifInterface
-import com.alvintio.pedulipangan.R
+import android.os.Bundle
 import com.alvintio.pedulipangan.databinding.ActivityExpirationCheckerBinding
-import com.alvintio.pedulipangan.ml.FoodPrediction
 import com.alvintio.pedulipangan.util.ViewUtils
-import org.tensorflow.lite.DataType
-import org.tensorflow.lite.support.image.TensorImage
-import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
-import java.io.File
-import java.io.FileOutputStream
-import java.io.InputStream
-import java.text.SimpleDateFormat
-import java.util.Locale
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
 class ExpirationCheckerActivity : AppCompatActivity() {
-
-    companion object {
-        private val REQUIRED_CAMERA_PERMISS = arrayOf(Manifest.permission.CAMERA)
-        private const val REQUEST_CODE_PERMISS = 101
-    }
-
     private lateinit var binding: ActivityExpirationCheckerBinding
-
-    private var getFile: File? = null
-
-    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == RESULT_OK) {
-            val selectedImageUri: Uri? = it.data?.data
-            selectedImageUri?.let { uri ->
-                getFile = File(getPathFromUri(uri))
-                binding.ivProductImage.setImageURI(uri)
-            }
-        }
-    }
-
-    private fun getPathFromUri(uri: Uri): String {
-        val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
-        val cursor = contentResolver.query(uri, filePathColumn, null, null, null)
-        cursor?.use {
-            if (it.moveToFirst()) {
-                val columnIndex = it.getColumnIndex(filePathColumn[0])
-                return it.getString(columnIndex)
-            }
-        }
-        return ""
-    }
-
-    private val cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-        if (it.resultCode == RESULT_OK) {
-            getFile?.let { image ->
-                val bitmap = BitmapFactory.decodeFile(image.path)
-                rotateImage(bitmap, image.path).compress(
-                    Bitmap.CompressFormat.JPEG,
-                    100,
-                    FileOutputStream(image)
-                )
-                binding.ivProductImage.setImageBitmap(rotateImage(bitmap, image.path))
-            }
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityExpirationCheckerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.apply {
-            btnCamera.setOnClickListener {
-                if (!checkImagePermission()) {
-                    ActivityCompat.requestPermissions(
-                        this@ExpirationCheckerActivity,
-                        REQUIRED_CAMERA_PERMISS,
-                        REQUEST_CODE_PERMISS
-                    )
-                } else {
-                    startCamera()
-                }
-            }
+        binding.btnBreadChecker.setOnClickListener {
+            val intent = Intent(this, ExpirationCheckerBreadActivity::class.java)
+            startActivity(intent)
+        }
 
-            btnGallery.setOnClickListener {
-                openGallery()
-            }
-
-            binding.startDetection.setOnClickListener {
-                startDetection()
-            }
+        binding.btnFruitChecker.setOnClickListener {
+            val intent = Intent(this, ExpirationCheckerFruitActivity::class.java)
+            startActivity(intent)
         }
 
         ViewUtils.setupFullScreen(this)
     }
-
-    private fun rotateImage(bitmap: Bitmap, path: String): Bitmap {
-        val orientation = ExifInterface(path).getAttributeInt(
-            ExifInterface.TAG_ORIENTATION,
-            ExifInterface.ORIENTATION_UNDEFINED
-        )
-        val matrix = Matrix()
-        when (orientation) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> matrix.setRotate(90f)
-            ExifInterface.ORIENTATION_ROTATE_180 -> matrix.setRotate(180f)
-        }
-
-        return Bitmap.createBitmap(
-            bitmap,
-            0,
-            0,
-            bitmap.width,
-            bitmap.height,
-            matrix,
-            true
-        )
-    }
-
-    private fun startCamera() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        intent.resolveActivity(packageManager)
-        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val customTempFile = File.createTempFile(
-            SimpleDateFormat(
-                "dd-MMM-yyyy",
-                Locale.US
-            ).format(System.currentTimeMillis()), ".jpg", storageDir
-        )
-        getFile = customTempFile
-        val photoURI: Uri = FileProvider.getUriForFile(
-            this@ExpirationCheckerActivity,
-            "com.alvintio.pedulipangan.fileprovider",
-            customTempFile
-        )
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-        cameraLauncher.launch(intent)
-    }
-
-    private fun openGallery() {
-        if (ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.READ_EXTERNAL_STORAGE
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
-            galleryLauncher.launch(intent)
-        } else {
-            ActivityCompat.requestPermissions(
-                this@ExpirationCheckerActivity,
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                REQUEST_CODE_PERMISS
-            )
-        }
-    }
-
-    private fun checkImagePermission() = REQUIRED_CAMERA_PERMISS.all {
-        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun startDetection() {
-        if (getFile != null) {
-            val model = FoodPrediction.newInstance(this)
-            val foodsDisease = getFoodDisease()
-
-            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 150, 150, 3), DataType.FLOAT32)
-
-            val bitmap = BitmapFactory.decodeFile(getFile?.path)
-            val resize = Bitmap.createScaledBitmap(bitmap, 150, 150, true)
-
-            val tensorImage = TensorImage(DataType.FLOAT32)
-            tensorImage.load(resize)
-
-            inputFeature0.loadBuffer(tensorImage.buffer)
-
-            val outputs = model.process(inputFeature0)
-            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
-
-            var maxIndex = 0
-            var maxValue = outputFeature0.getFloatValue(0)
-            for (i in 0 until 2) {
-                val value = outputFeature0.getFloatValue(i)
-                if (value > maxValue) {
-                    maxValue = value
-                    maxIndex = i
-                }
-            }
-
-            val foodDisease = foodsDisease[maxIndex]
-
-            binding.foodDisease.text = foodDisease
-
-        } else {
-            Toast.makeText(this, getString(R.string.input_img), Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun getFoodDisease(): List<String> {
-        val inputString = this.assets.open("labels_output.txt").bufferedReader().use {
-            it.readText()
-        }
-
-        return inputString.split("\n")
-    }
 }
-
---
+++ app/src/main/java/com/alvintio/pedulipangan/view/ExpirationCheckerBreadActivity.kt
@@ -0,0 +1,218 @@
+package com.alvintio.pedulipangan.view
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.net.Uri
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.exifinterface.media.ExifInterface
+import com.alvintio.pedulipangan.R
+import com.alvintio.pedulipangan.databinding.ActivityExpirationCheckerBinding
+import com.alvintio.pedulipangan.databinding.ActivityExpirationCheckerBreadBinding
+import com.alvintio.pedulipangan.ml.FoodPrediction
+import com.alvintio.pedulipangan.util.ViewUtils
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.File
+import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class ExpirationCheckerBreadActivity : AppCompatActivity() {
+
+    companion object {
+        private val REQUIRED_CAMERA_PERMISS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISS = 101
+    }
+
+    private lateinit var binding: ActivityExpirationCheckerBreadBinding
+
+    private var getFile: File? = null
+
+    private val galleryLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            val selectedImageUri: Uri? = it.data?.data
+            selectedImageUri?.let { uri ->
+                getFile = File(getPathFromUri(uri))
+                binding.ivProductImage.setImageURI(uri)
+            }
+        }
+    }
+
+    private fun getPathFromUri(uri: Uri): String {
+        val filePathColumn = arrayOf(MediaStore.Images.Media.DATA)
+        val cursor = contentResolver.query(uri, filePathColumn, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val columnIndex = it.getColumnIndex(filePathColumn[0])
+                return it.getString(columnIndex)
+            }
+        }
+        return ""
+    }
+
+    private val cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            getFile?.let { image ->
+                val bitmap = BitmapFactory.decodeFile(image.path)
+                rotateImage(bitmap, image.path).compress(
+                    Bitmap.CompressFormat.JPEG,
+                    100,
+                    FileOutputStream(image)
+                )
+                binding.ivProductImage.setImageBitmap(rotateImage(bitmap, image.path))
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExpirationCheckerBreadBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            btnCamera.setOnClickListener {
+                if (!checkImagePermission()) {
+                    ActivityCompat.requestPermissions(
+                        this@ExpirationCheckerBreadActivity,
+                        REQUIRED_CAMERA_PERMISS,
+                        REQUEST_CODE_PERMISS
+                    )
+                } else {
+                    startCamera()
+                }
+            }
+
+            btnGallery.setOnClickListener {
+                openGallery()
+            }
+
+            binding.startDetection.setOnClickListener {
+                startDetection()
+            }
+        }
+
+        ViewUtils.setupFullScreen(this)
+    }
+
+    private fun rotateImage(bitmap: Bitmap, path: String): Bitmap {
+        val orientation = ExifInterface(path).getAttributeInt(
+            ExifInterface.TAG_ORIENTATION,
+            ExifInterface.ORIENTATION_UNDEFINED
+        )
+        val matrix = Matrix()
+        when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> matrix.setRotate(90f)
+            ExifInterface.ORIENTATION_ROTATE_180 -> matrix.setRotate(180f)
+        }
+
+        return Bitmap.createBitmap(
+            bitmap,
+            0,
+            0,
+            bitmap.width,
+            bitmap.height,
+            matrix,
+            true
+        )
+    }
+
+    private fun startCamera() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        intent.resolveActivity(packageManager)
+        val storageDir: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val customTempFile = File.createTempFile(
+            SimpleDateFormat(
+                "dd-MMM-yyyy",
+                Locale.US
+            ).format(System.currentTimeMillis()), ".jpg", storageDir
+        )
+        getFile = customTempFile
+        val photoURI: Uri = FileProvider.getUriForFile(
+            this@ExpirationCheckerBreadActivity,
+            "com.alvintio.pedulipangan.fileprovider",
+            customTempFile
+        )
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+        cameraLauncher.launch(intent)
+    }
+
+    private fun openGallery() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            galleryLauncher.launch(intent)
+        } else {
+            ActivityCompat.requestPermissions(
+                this@ExpirationCheckerBreadActivity,
+                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                REQUEST_CODE_PERMISS
+            )
+        }
+    }
+
+    private fun checkImagePermission() = REQUIRED_CAMERA_PERMISS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startDetection() {
+        if (getFile != null) {
+            val model = FoodPrediction.newInstance(this)
+            val foodsDisease = getFoodDisease()
+
+            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 150, 150, 3), DataType.FLOAT32)
+
+            val bitmap = BitmapFactory.decodeFile(getFile?.path)
+            val resize = Bitmap.createScaledBitmap(bitmap, 150, 150, true)
+
+            val tensorImage = TensorImage(DataType.FLOAT32)
+            tensorImage.load(resize)
+
+            inputFeature0.loadBuffer(tensorImage.buffer)
+
+            val outputs = model.process(inputFeature0)
+            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+            var maxIndex = 0
+            var maxValue = outputFeature0.getFloatValue(0)
+            for (i in 0 until 2) {
+                val value = outputFeature0.getFloatValue(i)
+                if (value > maxValue) {
+                    maxValue = value
+                    maxIndex = i
+                }
+            }
+
+            val foodDisease = foodsDisease[maxIndex]
+
+            binding.foodDisease.text = foodDisease
+
+        } else {
+            Toast.makeText(this, getString(R.string.input_img), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun getFoodDisease(): List<String> {
+        val inputString = this.assets.open("labels_output.txt").bufferedReader().use {
+            it.readText()
+        }
+
+        return inputString.split("\n")
+    }
+}
+
